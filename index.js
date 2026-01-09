c3aaf1df6a3898b2f48b8b8e831272ac85de7356
Daniel Barreto <danielbarreto1236@gmail.com>
2026-01-09 16:15:08 -0300

    Added new functions, node_modules removed. Closest to final version (thats the definitive commit, the last one i havent added the changes to git

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ let contas=[
     {Nome: 'Pedro', Quantidade: 200}
 ];
 
- function Imprimir_Contas() {
+let contasBlock=[]
+
+function Imprimir_Contas() {
     console.log('Contas Disponíveis: ')
     for (let conta of contas){
     console.log(`----${conta.Nome}----`)
     console.log(`---- Quantidade: ${conta.Quantidade}----`)
+    console.log('--------------------------')
+
+ }
+};
+
+function Imprimir_Contas_Block() {
+    console.log('Contas Bloqueadas: ')
+    for (let contaB of contasBlock){
+    console.log(`----${contaB.Nome}----`)
+    console.log(`---- Quantidade: ${contaB.Quantidade}----`)
     console.log('--------------------------')
 
  }
@@ -25,7 +37,7 @@ let contas=[
 
  }
 
-async function init1() {
+async function init() {
     Imprimir_Contas()
 
     const a= await prompt('Gostaria de abrir uma nova conta? (S/N): ')
@@ -38,26 +50,111 @@ async function init1() {
         console.log('Conta Criada Com Sucesso! ')
         console.log('')
         Imprimir_Contas()
-        await To_Do()
+        await Menu()
     }
     else {
         console.log('')
         console.log('Opção Indisponílvel. Tente Novamente...')
         console.log('')
-        await init1();
+        await init();
     }
     
 };
 
-init1();
+init();
 
-async function To_Do(){
+async function Menu(){
     console.log(' ')
     console.log('Transferir um Valor (1)')
-    console.log('Bloquear um Usuário (2)')
+    console.log('Bloquear uma Conta (2)')
     console.log('Realizar uma Cobrança em Débito Automático (3)')
     const b= await prompt('O que Gostaria de Fazer? (1); (2); (3): ')
+    
+    if (b == 1) {
+        Transfer()
     }
+    if (b == 3) {
+        Transfer2()
+    }
+    if (b == 2) {
+        Block()
+    }
+    }
+
+async function Transfer(){
+    const contaUsuario = contas[contas.length - 1]
+    let nomeConta = await prompt (`Para qual conta gostaria de transferir, ${contaUsuario.Nome}? `)
+    const contaEntrada = contas.find((conta)=>conta.Nome === nomeConta)
+    let quantidadeTransferencia = await prompt(`Quanto deseja transferir? `) 
+    let quantidadeTransferenciaNum = parseFloat(quantidadeTransferencia)
+    
+    if(Number.isNaN(quantidadeTransferenciaNum)) {
+    console.log('Inválido')
+    console.log('')
+    quantidadeTransferencia = await prompt('Quanto deseja transferir? ')
+   } 
+   contaEntrada.Quantidade +=quantidadeTransferenciaNum
+   contaUsuario.Quantidade -=quantidadeTransferenciaNum
+   console.log(' ')
+   console.log('Transferência Realizada!')
+   console.log('')
+   Imprimir_Contas()
+   console.log('')
+   Menu()
+
+}
+
+async function Block(){
+    const contaUsuario = contas[contas.length - 1]
+    const usuarioBlock = await prompt (`Qual conta gostaria de bloquear, ${contaUsuario.Nome}? `)
+    const index = contas.findIndex(conta=>conta.Nome === usuarioBlock)
+        if (index === -1){
+            console.log('')
+            console.log('Conta não econtrada!')
+            console.log('')
+            return
+        }
+        if (usuarioBlock == contaUsuario.Nome){
+            console.log('')
+            console.log('Não é possível bloquear a si mesmo')
+            console.log('')
+            return
+        }
+    const [contaBloquear]= contas.splice(index, 1)
+    contasBlock.push(contaBloquear) 
+    console.log('')
+    console.log('Conta bloqueada com sucesso! ')
+    console.log('')
+    Imprimir_Contas()
+    console.log('')
+    Imprimir_Contas_Block()
+    console.log('')
+    Menu()
+
+}
+
+async function Transfer2(){
+    const contaUsuario = contas[contas.length - 1]
+    let nomeConta = await prompt (`Para qual conta gostaria de realizar uma cobrança, ${contaUsuario.Nome}? `)
+    const contaEntrada = contas.find((conta)=>conta.Nome === nomeConta)
+    let quantidadeTransferencia = await prompt(`Quanto deseja cobrar? `) 
+    let quantidadeTransferenciaNum = parseFloat(quantidadeTransferencia)
+    
+    if(Number.isNaN(quantidadeTransferenciaNum)) {
+    console.log('Inválido')
+    console.log('')
+    quantidadeTransferencia = await prompt('Quanto deseja cobrar? ')
+   } 
+   contaEntrada.Quantidade -=quantidadeTransferenciaNum
+   contaUsuario.Quantidade +=quantidadeTransferenciaNum
+   console.log(' ')
+   console.log('Cobrança Realizada!')
+   console.log('')
+   Imprimir_Contas()
+   console.log('')
+   Menu()
+
+}
 
 
 
